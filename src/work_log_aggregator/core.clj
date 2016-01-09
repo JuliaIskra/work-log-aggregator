c30952e799b0836672c6a04cd67930a59168cc46
Julia Nemtseva <juliaiskra@gmail.com>
2016-01-09 14:29:43 +0300

    Refactor and organize code

--- src/work_log_aggregator/core.clj
+++ src/work_log_aggregator/core.clj
@@ -2,44 +2,36 @@
   (:gen-class)
   (:require [clj-time.core :as t])
   (:require [clj-time.format :as f])
-  (:use clojure.pprint)
-  (:import (org.joda.time.format PeriodFormat)))
+  (:require [clojure.string :as s])
+  (:import (org.joda.time.format PeriodFormat))
+  (:import (org.joda.time DateTime Period)))
 
-(defn parse-date
-  [date-str]
-  (f/parse (f/formatter "yyyy-MM-dd HH:mm") date-str))
-
-(defn parse-entry
-  [entry-str]
-  (let [lines (clojure.string/split entry-str #"[\r\n]+")]
-    {:start-datetime (parse-date (get lines 0))
-     :end-datetime   (parse-date (get lines 2))
-     :task           (clojure.string/trim (get lines 1))}))
-
-(defn read-entries
-  [filename]
-  (map parse-entry (clojure.string/split (slurp filename) #"[\r\n]+ [\r\n]+")))
-
-(defn get-entry-day
-  [entry]
-  (.toDateMidnight (entry :start-datetime)))
-
-(defn group-by-task
-  [entries]
-  (group-by :task entries))
-
-(defn get-entry-interval
-  [entry]
-  (.toPeriod (t/interval (entry :start-datetime) (entry :end-datetime))))
-
-(defn sum-hours
-  [entries]
-  (reduce #(.plus %1 %2) (map get-entry-interval entries)))
 
 (defn map-second
   "Maps second element in a sequence of pairs"
   [f coll]
   (map (fn [[first second]] [first (f second)]) coll))
+
+
+(defrecord Entry [^DateTime start-datetime
+                  ^DateTime end-datetime
+                  ^String task])
+
+(defn get-entry-day
+  [^Entry entry]
+  (.toDateMidnight (:start-datetime entry)))
+
+(defn get-entry-interval
+  [entry]
+  (.toPeriod (t/interval (:start-datetime entry) (:end-datetime entry))))
+
+(defn group-by-task
+  [entries]
+  (group-by :task entries))
+
+(defn sum-hours
+  [entries]
+  (reduce #(.plus %1 %2) (map get-entry-interval entries)))
 
 (defn aggregate-by-date-task
   [entries]
@@ -48,32 +40,47 @@
        (map-second #(->> %1 group-by-task (map-second sum-hours)))
        (sort-by first)))
 
+
+(defn parse-date
+  [date-str]
+  (f/parse (f/formatter "yyyy-MM-dd HH:mm") date-str))
+
+(defn parse-entry
+  [entry-str]
+  (let [[start task end] (s/split entry-str #"[\r\n]+")]
+    (map->Entry {:start-datetime (parse-date start)
+                 :end-datetime   (parse-date end)
+                 :task           (s/trim task)})))
+
+(defn read-entries
+  [filename]
+  (map parse-entry (s/split (slurp filename) #"[\r\n]+ [\r\n]+")))
+
+
 (defn format-task-period
-  [[task period]]
+  [[task ^Period period]]
   (str
     task
     " - "
     (.print (PeriodFormat/getDefault) (.normalizedStandard period))))
 
-(defn format-entries
-  [entries]
-  (clojure.string/join "\n" (map format-task-period entries)))
-
 (defn format-aggregated-entries
   [aggregated-entries]
-  (clojure.string/join "\n\n"
-     (map
-       (fn [[day entries]]
-         (str
-           (f/unparse (f/formatters :date) day)
-           "\n"
-           (format-entries entries)))
-       aggregated-entries)))
+  (s/join
+    "\n\n"
+    (map
+      (fn [[day entries]]
+        (str
+          (f/unparse (f/formatters :date) day)
+          "\n"
+          (s/join "\n" (map format-task-period entries))))
+      aggregated-entries)))
 
-(defn print-hours
-  [aggregated-entries]
-  (println (format-aggregated-entries aggregated-entries)))
 
 (defn -main
-  [filename & args]
-  (-> filename read-entries aggregate-by-date-task print-hours))
+  [filename]
+  (-> filename
+      read-entries
+      aggregate-by-date-task
+      format-aggregated-entries
+      println))
