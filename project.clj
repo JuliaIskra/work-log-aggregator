025c5b2dc333464045f47bafccde49154d77d241
Julia Nemtseva <juliaiskra@gmail.com>
2016-01-16 18:26:49 +0300

    Update release settings

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject work_log_aggregator "0.1.0-SNAPSHOT"
+(defproject work_log_aggregator "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
@@ -6,4 +6,13 @@
   :dependencies [[org.clojure/clojure "1.7.0"][clj-time "0.11.0"]]
   :main ^:skip-aot work_log_aggregator.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version"
+                   "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"]
+                  ["uberjar"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]])
