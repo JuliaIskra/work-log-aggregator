a3f0bfa8d1690f2a8ff8f35c35ed2db6c121f642
Julia Nemtseva <juliaiskra@gmail.com>
2016-01-16 18:28:32 +0300

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject work_log_aggregator "0.1.0"
+(defproject work_log_aggregator "0.2.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
