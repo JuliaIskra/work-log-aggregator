461c042da6e7b174b1024b7a2e95853efe6599bc
Julia Nemtseva <juliaiskra@gmail.com>
2016-01-23 14:41:05 +0300

    Add documentation and comments for better understanding of the code

--- src/work_log_aggregator/core.clj
+++ src/work_log_aggregator/core.clj
@@ -30,23 +30,35 @@
   (group-by :task entries))
 
 (defn sum-hours
+  "Calculates total duration of all entries in passed sequence"
   [entries]
   (reduce #(.plus %1 %2) (map get-entry-duration entries)))
 
 (defn total-sum
+  "Adds total duration of all entries in the passed sequence as a new element
+  of the sequence"
   [entries]
   (conj entries ["total" (reduce #(.plus %1 %2) (map second entries))]))
 
 (defn aggregate-by-date-task
   [entries]
   (->> entries
+       ; group Entries by day
        (group-by get-entry-day)
+       ; group Entries by task for every day
        (map-second (fn [e]
                      (->> e
+                          ; returns sequence of (task sequnce-of-entries)
                           group-by-task
+                          ; sum hours for every task
+                          ; after this we have sequence of (task duration)
                           (map-second sum-hours)
+                          ; add total amount of time spent in the day
+                          ; as another task
                           total-sum
+                          ; sort by duration desc
                           (sort-by second #(compare %2 %1)))))
+       ; sort by day
        (sort-by first)))
 
 
@@ -61,10 +73,14 @@
                :task           (s/trim task)}))
 
 (defn read-entries
+  "Returns lazy sequence of Entries"
   [reader]
   (->> (line-seq reader)
        (map s/trim)
+       ; separate entries with empty lines
+       ; returns (("start-time" "task" "end-time")("")("start-time" "task" "end-time"))
        (partition-by s/blank?)
+       ; remove all sequences with all empty lines, i.e. ("")
        (remove #(every? s/blank? %1))
        (map parse-entry)))
 
