2c303e53f0916c2e8b4f5b3085277b4ef8220980
Julia Nemtseva <juliaiskra@gmail.com>
2016-02-13 21:30:12 +0300

    Add focus factor to output.

--- src/work_log_aggregator/core.clj
+++ src/work_log_aggregator/core.clj
@@ -17,11 +17,11 @@
                   ^DateTime end-datetime
                   ^String task])
 
-(defn get-entry-day
+(defn entry-day
   [^Entry entry]
   (.toDateMidnight (:start-datetime entry)))
 
-(defn get-entry-duration
+(defn entry-duration
   [entry]
   (.toDuration (t/interval (:start-datetime entry) (:end-datetime entry))))
 
@@ -32,47 +32,45 @@
 (defn sum-hours
   "Calculates total duration of all entries in passed sequence"
   [entries]
-  (reduce #(.plus %1 %2) (map get-entry-duration entries)))
+  (reduce #(.plus %1 %2) (map entry-duration entries)))
 
 (defn total-sum
-  "Adds total duration of all entries in the passed sequence as a new element
-  of the sequence"
+  "Calculates total duration of all entries"
   [entries]
-  (conj entries ["total" (reduce #(.plus %1 %2) (map second entries))]))
+  (reduce #(.plus %1 %2) (map second entries)))
 
-(defn calculate-focus-factor
-  "Calculates duration for each task taking into account focus factor"
-  [entries]
-  (let [total        (first entries)
-        day-duration (Duration/standardHours 8)
-        ff           (/
-                       (.getStandardMinutes (second total))
-                       (.getStandardMinutes day-duration))]
-    (map (fn [[task duration]]
-           [task
-            duration
-            (Duration/standardMinutes (/ (.getStandardMinutes duration) ff))])
-         entries)))
+(defn calculate-ff
+  [total-time]
+  (let [day-duration (Duration/standardHours 8)]
+    (/
+      (.getStandardMinutes total-time)
+      (.getStandardMinutes day-duration))))
+
+(defn add-ff-duration
+  [ff [task-name duration]]
+  [task-name
+   duration
+   (Duration/standardMinutes (/ (.getStandardMinutes duration) ff))])
 
 (defn aggregate-by-date-task
   [entries]
   (->> entries
-       ; group Entries by day
-       (group-by get-entry-day)
-       ; group Entries by task for every day
-       (map-second (fn [e]
-                     (->> e
-                          ; returns sequence of (task sequnce-of-entries)
-                          group-by-task
-                          ; sum hours for every task
-                          ; after this we have sequence of (task duration)
-                          (map-second sum-hours)
-                          ; add total amount of time spent in the day
-                          ; as another task
-                          total-sum
-                          ; sort by duration desc
-                          (sort-by second #(compare %2 %1))
-                          calculate-focus-factor)))
+       (group-by entry-day)
+       (map (fn [[day entries]]
+              (let [tasks          (->> entries
+                                        ; returns sequence of (task sequnce-of-entries)
+                                        group-by-task
+                                        ; sum hours for every task
+                                        ; after this we have sequence of (task duration)
+                                        (map-second sum-hours)
+                                        ; sort by duration in reverse order
+                                        (sort-by second #(compare %2 %1)))
+                    total-time     (total-sum tasks)
+                    ff             (calculate-ff total-time)
+                    tasks-ff-total (->> tasks
+                                        (cons ["total" total-time])
+                                        (map (partial add-ff-duration ff)))]
+                [day ff tasks-ff-total])))
        ; sort by day
        (sort-by first)))
 
@@ -110,14 +108,18 @@
     (.print (PeriodFormat/getDefault) (.toPeriod ff-duration))
     ")"))
 
+(defn format-ff
+  [ff]
+  (str "FF = " (int (* 100 ff)) "%"))
+
 (defn format-aggregated-entries
   [aggregated-entries]
   (->> aggregated-entries
        (map
-         (fn [[day entries]]
-           (cons
-             (f/unparse (f/formatters :date) day)
-             (map format-task-duration entries))))
+         (fn [[day ff entries]]
+           (concat [(f/unparse (f/formatters :date) day)
+                    (format-ff ff)]
+                   (map format-task-duration entries))))
        (interpose "")
        flatten))
 
@@ -125,6 +127,7 @@
   [coll]
   (doseq [s coll]
     (println s)))
+
 
 (defn -main
   [filename]
